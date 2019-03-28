802d737467ff930969e318a178a8fbe268b20457
matikos <mati.kos@me>
2019-03-28 15:10:12 +0000

    Initial Commit

---
+++ MyCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCalculator
+//
+//  Created by Your Host on 3/28/19.
+//  Copyright © 2019 Mati Kos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
